0061a7d8ccc2b0e9ce93677d16ffcde180040edc
ahlamalefishat96 <ahlam.alefishat@gmail.com>
2020-06-10 02:45:58 +0300

    lab-11

---
+++ src/auth/models/users/users-model.js
@@ -0,0 +1,36 @@
+'use strict';
+require('dotenv').config();
+const schema= require('./users-schema.js');
+const Model = require('../mongo.js');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const SECRET = process.env.SECRET || 'mysecret';
+
+class Users extends Model {
+  constructor(){
+    super(schema);
+  }
+  async save(record){
+    const db = await this.get ({username : record.username});
+    if (db.length == 0) {
+      record.password = await bcrypt.hash(record.password, 5);
+      const user = await this.create(record);
+      return user;
+    }
+    // return Promise.reject(); // ==>.catch
+  }
+  // user:pass
+  //signin
+  async authenticateBasic(user , pass){
+    const db = await this.get({username : user});
+    const valid = await bcrypt.compare(pass,db[0].password);
+    return valid ? db : Promise.reject('wrong password');
+  }
+  //signin/signup
+  generateToken(user){
+    const token = jwt.sign({ username :user.username}, SECRET);
+    return token;
+  }
+}
+
+module.exports = new Users();
