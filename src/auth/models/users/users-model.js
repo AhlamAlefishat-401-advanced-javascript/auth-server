322e00eb3952ed3bd8946d9f7ab266d950a7c7d6
ahlamalefishat96 <ahlam.alefishat@gmail.com>
2020-06-11 09:24:46 +0300

    lab-14

--- src/auth/models/users/users-model.js
+++ src/auth/models/users/users-model.js
@@ -6,6 +6,11 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const SECRET = process.env.SECRET || 'mysecret';
 
+let roles = {
+  user: ['read'],
+  editor: ['read', 'update', 'create'],
+  admin: ['read', 'update', 'create', 'delete'],
+};
 class Users extends Model {
   constructor(){
     super(schema);
@@ -28,9 +33,24 @@ class Users extends Model {
   }
   //signin/signup
   generateToken(user){
-    const token = jwt.sign({ username :user.username}, SECRET);
+    const token = jwt.sign({ username :user.username,
+      capabilities: roles[user.role],
+    }, SECRET);
     return token;
   }
 }
-
+Users.verifyToken = function (token) {
+  return jwt.verify(token, SECRET, function(err, decoded) {
+    if (err) {
+      console.log('err>>> ', err);
+      return Promise.reject(err);
+    }
+    console.log('decoded >>>> ',decoded); // {username: usernameValue, ...}
+    let username = decoded['username']; // decoded.username
+    if (db[username]) {
+      return Promise.resolve(decoded);
+    } 
+    return Promise.reject();
+  });
+};
 module.exports = new Users();
